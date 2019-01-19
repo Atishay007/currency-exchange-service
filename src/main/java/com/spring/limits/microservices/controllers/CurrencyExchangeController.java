73414770dd9f56b7ade9552f0cddbfbcb81a1dbb
Atishay Jain <atishayjain23@gmail.com>
2019-01-20 00:19:37 +0530

    Added Properties Files and comments

--- src/main/java/com/spring/limits/microservices/controllers/CurrencyExchangeController.java
+++ src/main/java/com/spring/limits/microservices/controllers/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package com.spring.limits.microservices.controllers;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
